f461feecd74a5a2b3ce9c9de5d903d5e3987fe18
CemBC <119740765+CemBC@users.noreply.github.com>
2024-04-13 22:28:32 +0300

    Creating classes

---
+++ main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+}
+
+class ImageCarousel extends StatefulWidget{
+  const ImageCarousel({super.key});
+
+  State<ImageCarousel> createState() => _ImageCarouselState();
+}
+
+class _ImageCarousel extends State<ImageCarousel> {
+  
+}
+
+
