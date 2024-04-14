8766c3162caacd339c2204680909b8ee64962b98
CemBC <119740765+CemBC@users.noreply.github.com>
2024-04-14 12:57:07 +0300

    Adding the image Column
    Wrapped all the widgets inside of the MaterialApp and Scaffold because flutter had an error about that

--- main.dart
+++ main.dart
@@ -36,5 +36,38 @@ class _ImageCarouselState extends State<ImageCarousel> {
       }
     });
   }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body:  Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      height: 500,
+                      width: 500,
+                      margin: EdgeInsets.all(20.00),
+                      padding: EdgeInsets.all(10.00),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blue, width: 5.0),
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            const BoxShadow(
+                                color: Colors.grey,
+                                spreadRadius: 10,
+                                blurRadius: 7,
+                                offset: Offset(0,3)
+                            )]
+                      ),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        child: Image.asset(_images[_index],
+                          fit: BoxFit.fitWidth,),)
+                  ),
+                  
+                ]
+            )
+        ));
+  }
 }
 
